b99b5eb21a0293318f4b7b9b5c2f950a409e11cd
miladmo68 <miladmo68@gmail.com>
2025-12-16 14:44:22 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const users = [
+    { id: 1, name: "Milad", age: 27 },
+    { id: 2, name: "Sara", age: 32 },
+    { id: 3, name: "John", age: 21 },
+  ];
+
+  return <div>hi</div>;
+}
+
+export default App;
